ead4d9c630b3e72c2a749aa36b26d5eb78a51bd7
John Tramm <john.tramm@gmail.com>
2014-01-10 12:25:00 -0600

    updated mem estimate

--- src/utils.c
+++ src/utils.c
@@ -16,11 +16,10 @@ double rn(unsigned long * seed)
 
 size_t get_mem_estimate( Input input )
 {
-	size_t E_levels = input.n_nuclides * input.n_resonances * sizeof(double) + input.n_nuclides * sizeof(double *);
-	size_t A_weights = input.n_nuclides * sizeof(double);
+	size_t radii = input.n_nuclides * sizeof(double);
 	size_t resonances = input.n_nuclides * input.n_resonances * sizeof(Resonance) + input.n_nuclides * sizeof(Resonance *);
 
-	size_t total = E_levels + A_weights + resonances;
+	size_t total = radii + resonances;
 	
 	return total;
 }
