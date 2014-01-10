a302f08cbedcd20382be25be2613101b69ca7aef
John Tramm <john.tramm@gmail.com>
2014-01-10 12:26:42 -0600

    dev

--- src/io.c
+++ src/io.c
@@ -166,10 +166,10 @@ void print_input_summary(Input input)
 	// Calculate Estimate of Memory Usage
 	size_t mem = get_mem_estimate(input);
 
-	printf("Threads:        %d\n", input.nthreads);
-	printf("Nuclides:       %d\n", input.n_nuclides);
-	printf("Lookups:        "); fancy_int(input.lookups);
-	printf("HM Size:        %d\n", input.HM);
-	printf("Ave Resonances: "); fancy_int(input.n_resonances);
-	printf("Mem Usage (MB): %.1lf\n", mem / 1024.0 / 1024.0);
+	printf("Threads:          %d\n", input.nthreads);
+	printf("Nuclides:         %d\n", input.n_nuclides);
+	printf("Lookups:          "); fancy_int(input.lookups);
+	printf("HM Size:          %d\n", input.HM);
+	printf("Ave Resonances:   "); fancy_int(input.n_resonances);
+	printf("Mem Usage (MB):   %.1lf\n", mem / 1024.0 / 1024.0);
 }
