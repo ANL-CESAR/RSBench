ff61e09b553d092450dbc7fef6a1a96741fff992
John Tramm <john.tramm@gmail.com>
2014-08-21 14:20:54 -0500

    revved to version 4

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(int argc, char * argv[])
 	// Initialization & Command Line Read-In
 	// =====================================================================
 
-	int version = 3;
+	int version = 4;
 	int max_procs = omp_get_num_procs();
 	double start, stop;
 	
