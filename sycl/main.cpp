6a40a07503b4c790e9f58330aa3c513f990fddd3
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-11-03 17:53:40 -0400

    SYCL2020 Updates
    * refactor: updates for SYCL2020
    
    * refactor: namespace cl::sycl to sycl
    
    * feat: update Makefile to target common backends
    
    * fix: namespace and initializations
    
    * refactor: rename rsbench to RSBench to match repo name
    
    * feat: synchronize with host using get_host_access
    
    ---------
    
    Co-authored-by: Pranav Sivaraman <14294205+PerryStyle@users.noreply.github.com>
    
    Free Host Memory for SYCL2020 (#6)
    
    * feat: synchronize with host using get_host_access
    
    * feat: release dynamic memory
    
    ---------
    
    Co-authored-by: Pranav Sivaraman <14294205+PerryStyle@users.noreply.github.com>
    
    Fix offload Arch instead of sm version (#7)
    
    * refactor: updates for SYCL2020
    
    * refactor: namespace cl::sycl to sycl
    
    * feat: update Makefile to target common backends
    
    * fix: namespace and initializations
    
    * refactor: rename rsbench to RSBench to match repo name
    
    * feat: synchronize with host using get_host_access
    
    * feat: release dynamic memory
    
    * fix: OFFLOAD_ARCH instead of SM_VERSION
    
    ---------
    
    Co-authored-by: Pranav Sivaraman <14294205+PerryStyle@users.noreply.github.com>

--- sycl/main.cpp
+++ sycl/main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char * argv[])
 	// Run Simulation
 	start = get_time();
 
-	double kernel_init_time;
+	double kernel_init_time = 0;
 
 	// Run simulation
 	if( input.simulation_method == EVENT_BASED )
@@ -72,6 +72,8 @@ int main(int argc, char * argv[])
 	vhash = vhash % 999983;
 
 	printf("Simulation Complete.\n");
+
+    release_memory(SD);
 
 	// =====================================================================
 	// Print / Save Results and Exit
